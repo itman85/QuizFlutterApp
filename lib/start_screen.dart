f80765f561bc8159b99d2bd2b9d8e34d250e400e
Phan Nguyen <phannguyen@swissmarketplace.group>
2024-05-26 09:08:51 +0700

    deco button with icon and change image opacity

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
@@ -12,21 +13,30 @@ class StartScreen extends StatelessWidget {
         Image.asset(
           'assets/images/quiz-logo.png',
           width: 300,
+          color: const Color.fromARGB(150, 250, 250, 250),
         ),
+        /* Opacity(
+          opacity: 0.5,
+          child: Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+          ),
+        ), */ // use Opacity will be low performance
         const SizedBox(
           height: 32,
         ),
         const Text(
           'Learn Flutter',
-          style: TextStyle(color: Colors.white60, fontSize: 24),
+          style: TextStyle(color: Colors.white, fontSize: 24),
         ),
         const SizedBox(
           height: 32,
         ),
-        OutlinedButton(
+        OutlinedButton.icon(
             onPressed: () {},
             style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
-            child: const Text('Start Quiz'))
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text('Start Quiz'))
       ],
     ));
   }
