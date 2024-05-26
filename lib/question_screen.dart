139e29173280eea6448fcce470bac72a9c2c06db
Phan Nguyen <phannguyen@swissmarketplace.group>
2024-05-26 11:09:08 +0700

    align column content center, load list content dynamically from data, create re-used widget

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quizflutterapp/answer_button.dart';
+import 'package:quizflutterapp/data/questions.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -11,6 +13,26 @@ class QuestionScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text("Question screen");
+    final currentQuestion = questions[0];
+
+    return SizedBox(
+      width: double.infinity, // infinity: use as much width as it can
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(color: Colors.white),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          // spreding values (...) add a list as multiple individual values into the container list
+          ...currentQuestion.answers.map((item) {
+            return AnswerButton(answerText: item, onTap: () {});
+          })
+        ],
+      ),
+    );
   }
 }
